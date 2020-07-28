1967d6029a87aed7e5047598773052da95edb23b
Jackson Wolf <jtw3vhz@virginia.edu>
2020-07-27 22:03:54 -0400

    Updates

---
+++ precache-manifest.07e5ac6abd97827d4c3c5d299ca20fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599d682b23e9fc41694b3fa8003d60c5",
+    "url": "/CookBook/index.html"
+  },
+  {
+    "revision": "18459a766b1bb0cda5df",
+    "url": "/CookBook/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "b024261f369dfb526731",
+    "url": "/CookBook/static/css/main.ac6f2715.chunk.css"
+  },
+  {
+    "revision": "18459a766b1bb0cda5df",
+    "url": "/CookBook/static/js/2.cb4ab081.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CookBook/static/js/2.cb4ab081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b024261f369dfb526731",
+    "url": "/CookBook/static/js/main.ae680727.chunk.js"
+  },
+  {
+    "revision": "5f731871bea1880fa640",
+    "url": "/CookBook/static/js/runtime-main.173648b5.js"
+  }
+]);
